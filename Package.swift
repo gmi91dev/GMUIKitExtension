d70d33755594c1da3791a6093f21e9981c0dab54
Gmi91 <gmi91.dev@gmail.com>
2025-05-07 18:00:47 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GMUIKitExtension"),
+            name: "GMUIKitExtension", path: "Sources"),
         .testTarget(
             name: "GMUIKitExtensionTests",
             dependencies: ["GMUIKitExtension"]
